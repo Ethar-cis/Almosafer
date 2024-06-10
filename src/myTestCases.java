db9ebef77f773517166e8a7a915eefa5e3a79e2d
ethar <etharababneh1@gmail.com>
2024-06-10 23:16:04 +0300

    update3

--- src/myTestCases.java
+++ src/myTestCases.java
@@ -1,4 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class myTestCases {
-
+	WebDriver driver = new ChromeDriver();
+	String theURL = "https://global.almosafer.com/en"; 
+	
+	
+	@BeforeTest
+	public void mySetUp() {
+		driver.get(theURL);
+		
+	}
+	
+	
+	 @Test
+	 public void firstTest() {
+		 System.out.println("hii");
+	 }
 }
